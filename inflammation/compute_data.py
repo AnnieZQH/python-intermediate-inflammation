8cdaefd8c5e703bfd69c9e6e8cbd90d5e17ccd9c
Annie Zheng <annie.zheng@manchester.ac.uk>
2025-10-16 21:22:35 +0100

    Add regression test

--- inflammation/compute_data.py
+++ inflammation/compute_data.py
@@ -32,9 +32,9 @@ class CSVDataSource:
         self.dir_path = dir_path
     
     def load_inflammation_data(self):
-        data_file_paths = glob.glob(os.path.join(self.data_dir, 'inflammation*.csv'))
+        data_file_paths = glob.glob(os.path.join(self.dir_path, 'inflammation*.csv'))
         if len(data_file_paths) == 0:
-            raise ValueError(f"No inflammation data CSV files found in path {self.data_dir}")
+            raise ValueError(f"No inflammation data CSV files found in path {self.dir_path}")
         data = map(models.load_csv, data_file_paths)
         return list(data)
 
