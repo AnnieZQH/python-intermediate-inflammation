716eb8c9d3e2d745c7dd2086b996a7d8f6976ee7
Annie Zheng <annie.zheng@manchester.ac.uk>
2025-10-16 21:10:02 +0100

    Add compute_data test file

---
+++ tests/test_compute_data.py
@@ -0,0 +1,11 @@
+from unittest.mock import Mock
+
+def test_analyse_data_mock_source():
+    from inflammation.compute_data import analyse_data
+    data_source = Mock()
+
+    # data_source mock
+    mock_data = [[[0, 2, 0]], [[0, 1, 0]]]
+    data_source.load_inflammation_data.return_value = mock_data
+
+    analyse_data(data_source)
